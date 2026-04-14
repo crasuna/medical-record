f27545f33c98a2a3ebf0bb64772ea13453539158
love_luke <2449350454@qq.com>
2026-04-14 17:37:40 +0800

    fix: use bitmap launcher icons for device compatibility

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.crasuna.medicalrecord"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 6
+        versionName = "1.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
